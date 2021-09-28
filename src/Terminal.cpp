ffa665d02a777e6986570aa8f4ee778f31e13408
Radek Brich <radek.brich@devl.cz>
2021-09-28 11:19:10 +0200

    cleanup: shorter copyright preamble, add LICENSE

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -1,17 +1,7 @@
-// Terminal.cpp created on 2018-07-28, part of XCI term
-// Copyright 2018 Radek Brich
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Terminal.cpp created on 2018-07-28
+// This file is part of Termic project <https://github.com/rbrich/termic>
+// Copyright 2018–2021 Radek Brich
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
 
 #include "Terminal.h"
 #include "utility.h"
--- src/utility.h
+++ src/utility.h
@@ -1,17 +1,7 @@
-// utility.h created on 2018-07-30, part of XCI term
-// Copyright 2018 Radek Brich
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// utility.h created on 2018-07-30
+// This file is part of Termic project <https://github.com/rbrich/termic>
+// Copyright 2018–2021 Radek Brich
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
 
 #ifndef XCITERM_UTILITY_H
 #define XCITERM_UTILITY_H
